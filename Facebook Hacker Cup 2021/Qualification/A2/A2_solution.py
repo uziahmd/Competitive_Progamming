892131be94c013f72305e533f43f2eb5bc754403
Uzair Ahmed <uzi.ahmed12@gmail.com>
2021-08-29 02:31:32 +0500

    trying shiz for A2

--- Facebook Hacker Cup 2021/Qualification/A2/A2_solution.py
+++ Facebook Hacker Cup 2021/Qualification/A2/A2_solution.py
@@ -1,19 +1,75 @@
-def trans_combi(line, trans):
-    trans_copy = trans[:]
-    all_combin = []
-    theset =set()
-    print(trans)
-    for x in trans:
-        theset.add(x[0])
-    print(theset)
+def turns(line, trans, common):
+    result = []
+    print(common)
+    for a in common:
+        paths = []
+        temp = []
+        temp.append(a)
+        paths.append(temp)
+        count = 0
+        for x in trans:
+            if temp[-1] == x[1]:
+                count = count + 1
+        for x in range(count-1):
+            paths.append(temp)
+        
+        for current in paths:
+            for x in trans:
+                if x[1] == current[-1]:
+                    current.append(x[0])
+        print(paths)
+            # count = 0
+            # for current in paths:
+            #     if x[1] == current[-1]:
+            #         if count == 0:
+            #             current.append(x[0])
+            #             count = count + 1
+            #         else:
+            #             print("yeet")
+            #             paths.append(current)
+        #print(paths)
 
+def possible(line, trans):
+    all_combin = []
+    the_set =set()
+    for x in line:
+        the_set.add(x)
+    for x in the_set:
+        temp = []
+        temp.append(x)
+        all_combin.append(temp)
+    
+    for a in trans:
+        for x in all_combin:
+            if a[0] in x:
+                x.append(a[1])
+    for a in all_combin:
+        for x in all_combin:
+            if a[0] in x:
+                x.extend(a)
+    
+    combin_set = []
+    for x in all_combin:
+        combin_set.append(set(x))
+    
+    common = combin_set[0]
+    for x in combin_set[1:]:
+        common.intersection_update(x)
+    # print(the_set)
+    # print(combin_set)
+    return common
 
 
 def min_time(line, n_trans, trans):
     length = len(line)
     print(line)
-    print(n_trans)
-    trans_combi(line, trans)
+    common = possible(line, trans)
+    if len(common) == 0:
+        print("-1")
+    elif(n_trans == 0):
+        print(0)
+    else: 
+        turns(line, trans, common)
 
 with open('input.txt','r') as inf:
     T = int(inf.readline())
