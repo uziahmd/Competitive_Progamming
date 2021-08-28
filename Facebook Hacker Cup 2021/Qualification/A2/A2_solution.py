0e758143dbb136818326a4d547efd0dd50a4d476
Uzair Ahmed <uzi.ahmed12@gmail.com>
2021-08-28 23:18:30 +0500

    Renamed the folders kunkai I am dumb AF

--- Facebook Hacker Cup 2021/Qualification/A2/A2_solution.py
+++ Facebook Hacker Cup 2021/Qualification/A2/A2_solution.py
@@ -1,9 +1,19 @@
-def min_time(line, n_tras, trans):
+def trans_combi(line, trans):
+    trans_copy = trans[:]
+    all_combin = []
+    theset =set()
+    print(trans)
+    for x in trans:
+        theset.add(x[0])
+    print(theset)
+
+
+
+def min_time(line, n_trans, trans):
     length = len(line)
     print(line)
-    print(length)
     print(n_trans)
-    print(trans)
+    trans_combi(line, trans)
 
 with open('input.txt','r') as inf:
     T = int(inf.readline())
@@ -11,8 +21,8 @@ with open('input.txt','r') as inf:
         line = inf.readline().strip()
         n_trans = int(inf.readline())
         trans = []
-        for x in range(0, n_trans):
-            trans[x] = inf.readline().strip()
+        for x in range(n_trans):
+            trans.append(inf.readline().strip())
 
         min_time(line, n_trans, trans)
         # with open('output.txt', 'a') as outf:
